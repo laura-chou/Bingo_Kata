8eb3860c4a9525e322f8f769a577c5fb51755272
Yu-Lun <chou522128@gmail.com>
2023-11-26 23:39:22 +0800

    [Green] BingoCard.cs：return with B scope integer

--- Bingo/Bingo/src/BingoCard.cs
+++ Bingo/Bingo/src/BingoCard.cs
@@ -4,7 +4,14 @@
     {
         public Column CreateColumnB()
         {
-            throw new NotImplementedException();
+            return new Column
+            {
+                Column1 = 10,
+                Column2 = 2,
+                Column3 = 1,
+                Column4 = 11,
+                Column5 = 15
+            };
         }
     }
 }
