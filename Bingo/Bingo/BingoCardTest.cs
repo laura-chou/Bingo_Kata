d7549a6c0faeacb813105da5532d9733d9f8daed
Yu-Lun <chou522128@gmail.com>
2023-12-02 15:33:35 +0800

    [Refactor]  BingoCardTest.cs：extract ValidateColumnBNumber method

--- Bingo/Bingo/BingoCardTest.cs
+++ Bingo/Bingo/BingoCardTest.cs
@@ -14,13 +14,22 @@ namespace Bingo
             var actual = bingoCard.CreateColumnB();
             var expected = new Column
             {
-                Column1 = actual.Column1,
-                Column2 = actual.Column2,
-                Column3 = actual.Column3,
-                Column4 = actual.Column4,
-                Column5 = actual.Column5
+                Column1 = ValidateColumnBNumber(actual.Column1) ? actual.Column1 : 0,
+                Column2 = ValidateColumnBNumber(actual.Column2) ? actual.Column2 : 0,
+                Column3 = ValidateColumnBNumber(actual.Column3) ? actual.Column3 : 0,
+                Column4 = ValidateColumnBNumber(actual.Column4) ? actual.Column4 : 0,
+                Column5 = ValidateColumnBNumber(actual.Column5) ? actual.Column5 : 0
             };
             actual.Should().BeEquivalentTo(expected);
+        }
+
+        private bool ValidateColumnBNumber(int columnNumber)
+        {
+            if (columnNumber >= 1 && columnNumber <= 15)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
