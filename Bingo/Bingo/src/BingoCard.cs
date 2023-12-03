6c75bd5add479add7d2658769cb6f98e11d34b15
Yu-Lun <chou522128@gmail.com>
2023-12-03 22:33:45 +0800

    [Green] BingoCard.cs：get column N random numbers

--- Bingo/Bingo/src/BingoCard.cs
+++ Bingo/Bingo/src/BingoCard.cs
@@ -26,7 +26,23 @@ namespace Bingo.src
 
         public SpecialColumn CreateColumnN()
         {
-            throw new NotImplementedException();
+            var randomNumberList = Enumerable.Range(31, 15)
+                .OrderBy(x => _random.Next())
+                .Take(4)
+                .ToList();
+
+            return GetSpecialColumn(randomNumberList);
+        }
+
+        private SpecialColumn GetSpecialColumn(List<int> randomNumberList)
+        {
+            return new SpecialColumn
+            {
+                Column1 = randomNumberList[0],
+                Column2 = randomNumberList[1],
+                Column4 = randomNumberList[2],
+                Column5 = randomNumberList[3]
+            };
         }
 
         private Column GetColumn(List<int> randomNumberList)
