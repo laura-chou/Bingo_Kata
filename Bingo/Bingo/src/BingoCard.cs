213df506e6ad54d4c5fd2fd95558fc119565d218
Yu-Lun <chou522128@gmail.com>
2023-12-03 21:47:38 +0800

    [Refactor] BingoCard.cs：refactor code

--- Bingo/Bingo/src/BingoCard.cs
+++ Bingo/Bingo/src/BingoCard.cs
@@ -2,31 +2,30 @@ namespace Bingo.src
 {
     public class BingoCard
     {
+        private Random _random = new Random();
+
         public Column CreateColumnB()
         {
-            Random random = new Random();
             var randomNumberList = Enumerable.Range(1, 15)
-                .OrderBy(x => random.Next())
+                .OrderBy(x => _random.Next())
                 .Take(5)
                 .ToList();
 
-            return new Column
-            {
-                Column1 = randomNumberList[0],
-                Column2 = randomNumberList[1],
-                Column3 = randomNumberList[2],
-                Column4 = randomNumberList[3],
-                Column5 = randomNumberList[4]
-            };
+            return GetColumn(randomNumberList);
         }
+
         public Column CreateColumnI()
         {
-            Random random = new Random();
             var randomNumberList = Enumerable.Range(16, 15)
-                .OrderBy(x => random.Next())
+                .OrderBy(x => _random.Next())
                 .Take(5)
                 .ToList();
 
+            return GetColumn(randomNumberList);
+        }
+
+        private Column GetColumn(List<int> randomNumberList)
+        {
             return new Column
             {
                 Column1 = randomNumberList[0],
