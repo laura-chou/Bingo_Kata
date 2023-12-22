32cdc9e0b4ca34b05b03d7200bb3a092cc3cf8b6
Yu-Lun <chou522128@gmail.com>
2023-12-22 23:02:53 +0800

    [Red] BingoCardTest.cs - A05_CreateORandomNumber：add test case

--- Bingo/Bingo/BingoNumberTest.cs
+++ Bingo/Bingo/BingoNumberTest.cs
@@ -46,6 +46,14 @@ namespace Bingo
             var expected = ValidateColumnNumber("G", actual) ? actual : 0;
             actual.Should().Be(expected);
         }
+        
+        [Test]
+        public void A05_CreateORandomNumber()
+        {
+            var actual = _bingoNumber.CreateRandomNumber("O");
+            var expected = ValidateColumnNumber("O", actual) ? actual : 0;
+            actual.Should().Be(expected);
+        }
 
         private bool ValidateColumnNumber(string columnName, int columnNumber)
         {
@@ -54,7 +62,8 @@ namespace Bingo
                 { "B", new Tuple<int, int>((int)Minimum.B, (int)Maximum.B) },
                 { "I", new Tuple<int, int>((int)Minimum.I, (int)Maximum.I) },
                 { "N", new Tuple<int, int>((int)Minimum.N, (int)Maximum.N) },
-                { "G", new Tuple<int, int>((int)Minimum.G, (int)Maximum.G) }
+                { "G", new Tuple<int, int>((int)Minimum.G, (int)Maximum.G) },
+                { "O", new Tuple<int, int>((int)Minimum.O, (int)Maximum.O) }
             };
 
             if (columnNumber >= columnRange[columnName].Item1 
