00db7d29b20161b35d6d3a6e7a4da49ba4645cfa
Yu-Lun <chou522128@gmail.com>
2024-01-07 21:35:04 +0800

    [Green] BingoLine.cs：get bingo card vertical line and compare with bingo number

--- Bingo/Bingo/src/BingoLine.cs
+++ Bingo/Bingo/src/BingoLine.cs
@@ -4,6 +4,18 @@
     {
         public int GetBingoLine(BingoCard bingoCard, List<string> bingoNumber)
         {
+            var verticalLineList = new List<string>
+            {
+                "B" + bingoCard.B.Column1.ToString(),
+                "B" + bingoCard.B.Column2.ToString(),
+                "B" + bingoCard.B.Column3.ToString(),
+                "B" + bingoCard.B.Column4.ToString(),
+                "B" + bingoCard.B.Column5.ToString(),
+            };
+            if (bingoNumber.SequenceEqual(verticalLineList))
+            {
+                return 1;
+            }
             return 0;
         }
     }
